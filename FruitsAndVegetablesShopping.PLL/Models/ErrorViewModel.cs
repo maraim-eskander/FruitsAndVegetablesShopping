d1647aa1c1265cb6180579b4c5a40b2ab2d737ac
Mariam Eskander <mariameskander10@gmail.com>
2025-07-27 17:11:38 +0300

    Add project files.

---
+++ FruitsAndVegetablesShopping.PLL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FruitsAndVegetablesShopping.PLL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
