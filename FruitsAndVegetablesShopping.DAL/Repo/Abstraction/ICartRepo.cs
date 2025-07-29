557ba2c7117255e7ce09e198867840c10ede8d8b
Moaz715 <moazfathy592005@gmail.com>
2025-07-29 20:00:37 +0300

    Create ICartRepo.cs

---
+++ FruitsAndVegetablesShopping.DAL/Repo/Abstraction/ICartRepo.cs
@@ -0,0 +1,18 @@
+﻿using FruitsAndVegetablesShopping.DAL.Entities;
+
+namespace FruitsAndVegetablesShopping.DAL.Repo.Abstraction{
+public interface ICartRepo
+{
+    (bool, string?) Create(Cart cart);
+
+    //(bool, string) Edit(); //ynf3 aslan a3ml edit l Cart?
+
+    (Cart?, string?) GetById(int id);
+
+    (List<Cart>?, string?) GetAll();
+
+    (bool, string?) Delete(int id);
+
+    
+}
+}
