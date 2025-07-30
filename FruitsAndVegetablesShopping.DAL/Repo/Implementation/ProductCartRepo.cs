4af4bbbf87483a0a2d40c2e89ce182f6f9cdeb1d
Moaz715 <moazfathy592005@gmail.com>
2025-07-30 16:46:13 +0300

    Create ProductCartRepo.cs

---
+++ FruitsAndVegetablesShopping.DAL/Repo/Implementation/ProductCartRepo.cs
@@ -0,0 +1,117 @@
+using FruitsAndVegetablesShopping.DAL.Database;
+using FruitsAndVegetablesShopping.DAL.Entities;
+using FruitsAndVegetablesShopping.DAL.Repo.Abstraction;
+namespace FruitsAndVegetablesShopping.DAL.Repo.Implementation
+{
+public class ProductCartRepo : IProductCartRepo
+{
+    private readonly Context db;
+
+    public ProductCartRepo(Context db)
+    {
+        this.db = db;
+    }
+    public (bool, string?) Create(ProductCart productCart)
+    {
+        try
+        {
+            db.productCarts.Add(productCart);
+            db.SaveChanges();
+            return (true, null);
+
+        }
+        catch (Exception ex)
+        {
+            return (false, ex.Message);
+        }
+    }
+
+    public (bool, string?) Delete(int id, string deletedBy)
+    {
+        try
+        {
+            var res = db.productCarts.Where(po => po.ProductCartId == id).FirstOrDefault();
+
+            if (res == null)
+            {
+                return (false, "Does not exist");
+            }
+
+            res.delete(deletedBy);
+            db.SaveChanges();
+            return (true, null);
+
+        }
+        catch (Exception ex)
+        {
+            return (false, ex.Message);
+        }
+    }
+
+    public (List<ProductCart>?, string?) GetAll()
+    {
+        try
+        {
+            var res = db.productCarts.ToList();
+
+            if (res == null || res.Count == 0)
+            {
+                return (null, "Empty OrderProducts Table");
+            }
+
+            return (res, null);
+        }
+        catch (Exception ex)
+        {
+            return (null, ex.Message);
+        }
+    }
+
+    public (ProductCart?, string?) GetById(int id)
+    {
+        try
+        {
+            var res = db.productCarts.Where(po => po.ProductCartId == id).FirstOrDefault();
+                
+            if (res == null)
+            {
+                return (null, "Does not exist");
+            }
+
+
+            return (res, null);
+
+        }
+        catch (Exception ex)
+        {
+            return (null, ex.Message);
+        }
+    }
+
+    /*
+    public (bool, string?) Update(int id, int qty)
+    {
+        try
+        {
+            var res = db.productCarts.Where(po => po.ProductCartId == id).FirstOrDefault();
+
+            if (res == null)
+            {
+                return (false, "Does not exist");
+            }
+
+            res.edit(qty);
+
+            return (true, null);
+
+        }
+        catch (Exception ex)
+        {
+            return (false, ex.Message);
+        }
+    }
+    */
+}
+
+
+}
