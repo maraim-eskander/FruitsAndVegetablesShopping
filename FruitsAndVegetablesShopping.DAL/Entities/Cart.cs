09bf2e8eac95bd1d127fa58c66250febd8a2911e
Moaz715 <moazfathy592005@gmail.com>
2025-08-02 13:45:39 +0300

    Update Cart.cs

--- FruitsAndVegetablesShopping.DAL/Entities/Cart.cs
+++ FruitsAndVegetablesShopping.DAL/Entities/Cart.cs
@@ -2,31 +2,43 @@
 namespace FruitsAndVegetablesShopping.DAL.Entities
 {
     public class Cart
+{
+    public int Cart_id { get; private set; }
+
+    public int numOfItems { get; private set; }
+
+
+    [Required, ForeignKey("Customer")]
+    public int CustomerId { get; private set; }
+
+    
+    public Customer Customer { get; private set; }
+
+    public List<ProductCart> ProductCarts { get; private set; } = new();
+
+
+    [Required]
+    public DateTime CreatedOn { get; private set; } = DateTime.Now;
+
+    public string? CreatedBy { get; private set; }
+
+    public DateTime? ModifiedOn { get; private set; }
+
+    public string? ModifiedBy { get; private set; }
+
+    public bool isDeleted { get; private set; } = false;
+
+    public DateTime? DeletedOn { get; private set; }
+
+    public string? DeletedBy { get; private set; }
+
+    //public Cart(int id, )
+
+    public void delete()
     {
-        public int Cart_id { get; private set; }
+        this.isDeleted = true;
+        this.DeletedOn = DateTime.Now;
 
-        public int numOfItems { get; private set; }
-
-        [Required]
-        public DateTime CreatedOn { get; private set; } = DateTime.Now;
-
-        public string? CreatedBy { get; private set; }
-
-        public DateTime? ModifiedOn { get; private set; }
-
-        public string? ModifiedBy { get; private set; }
-
-        public bool isDeleted { get; private set; } = false;
-
-        public DateTime? DeletedOn { get; private set; }
-
-        public string? DeletedBy { get; private set; }
-
-        public void delete()
-        {
-            this.isDeleted = true;
-            this.DeletedOn = DateTime.Now;
-
-        }
     }
+}
 }
