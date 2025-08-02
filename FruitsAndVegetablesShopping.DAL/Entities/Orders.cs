50cdefee9be793d964b5e18be270bbda7492e290
Moaz715 <moazfathy592005@gmail.com>
2025-08-02 13:35:30 +0300

    Update Orders.cs

--- FruitsAndVegetablesShopping.DAL/Entities/Orders.cs
+++ FruitsAndVegetablesShopping.DAL/Entities/Orders.cs
@@ -3,41 +3,69 @@ using System.ComponentModel.DataAnnotations;
 namespace FruitsAndVegetablesShopping.DAL.Entities
 {
     public class Orders
+{
+    public int Order_id { get; private set; }
+
+    public float totalPrice { get; private set; }
+
+    public int numOfItems { get; private set; }
+
+    [ForeignKey("customer")]
+    public int Customer_id { get; private set; }
+
+    public Customer customer { get; private set; }
+
+    [ForeignKey("delivery")]
+    public int Delivery_id { get; private set; }
+
+    public Delivery delivery { get; private set; }
+
+    public List<ProductOrder> ProductOrders { get; private set; } = new List<ProductOrder>();
+
+    [Required]
+    public DateTime CreatedOn { get; private set; } = DateTime.Now;
+
+    public string? CreatedBy { get; private set; }
+
+    public DateTime? ModifiedOn { get; private set; }
+
+    public string? ModifiedBy { get; private set; }
+
+    public bool isDeleted { get; private set; } = false;
+
+    public DateTime? DeletedOn { get; private set; }
+
+    public string? DeletedBy { get; private set; }
+
+    public Orders(float totalPrice, int numOfItems, int customer_id, int delivery_id, List<ProductOrder> productOrders)
     {
-        public int Order_id { get; private set; }
-
-        public float totalPrice { get; private set; }
-
-        public int numOfItems { get; private set; }
-        [Required]
-        public  Delivery delivery { get; private set; }
-
-        [Required]
-        public DateTime CreatedOn { get; private set; } = DateTime.Now;
-
-        public string? CreatedBy { get; private set; }
-
-        public DateTime? ModifiedOn { get; private set; }
-
-        public string? ModifiedBy { get; private set; }
-
-        public bool isDeleted { get; private set; } = false;
-
-        public DateTime? DeletedOn { get; private set; }
-
-        public string? DeletedBy { get; private set; }
-
-        public void delete()
-        {
-            this.isDeleted = true;
-            this.DeletedOn = DateTime.Now;
-
-        }
-
-        public void edit(float totalprice, int num)
-        {
-            this.totalPrice = totalprice;
-            this.numOfItems = num;
-        }
+        
+        this.totalPrice = totalPrice;
+        this.numOfItems = numOfItems;
+        Delivery_id = delivery_id;
+        ProductOrders = productOrders;
+        this.CreatedOn = DateTime.Now;
     }
+
+    public void delete(string deletedBy)
+    {
+        this.isDeleted = true;
+        this.DeletedOn = DateTime.Now;
+        this.DeletedBy = deletedBy;
+        if (ProductOrders != null)
+        {
+            foreach (var po in ProductOrders)
+            {
+                po.delete(deletedBy);
+            }
+        }
+
+    }
+
+    public void edit(float totalprice, int num)
+    {
+        this.totalPrice = totalprice;
+        this.numOfItems = num;
+    }
+}
 }
