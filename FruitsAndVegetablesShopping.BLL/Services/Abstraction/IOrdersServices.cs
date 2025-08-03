2e6f2e935a2ab811ba33174659d453369aa65f9c
Moaz715 <moazfathy592005@gmail.com>
2025-08-03 23:32:15 +0300

    Create IOrdersServices.cs

---
+++ FruitsAndVegetablesShopping.BLL/Services/Abstraction/IOrdersServices.cs
@@ -0,0 +1,16 @@
+﻿using FruitsAndVegetablesShopping.BLL.ModelVm.Product;
+namespace FruitsAndVegetablesShopping.BLL.Services.Abstraction
+{
+public interface IOrdersServices
+{
+    public Task<(bool, string)> CreateAsync(OrdersDTO ordersDTO);
+
+    public Task<(bool, string?)> EditAsync(OrdersDTO orderDTO);
+
+    public Task<(Orders?, string?)> GetByIdAsync(int id);
+
+    public Task<(List<Orders>?, string?)> GetAllAsync();
+
+    public Task<(bool, string?)> DeleteAsync(int id);
+}
+}
