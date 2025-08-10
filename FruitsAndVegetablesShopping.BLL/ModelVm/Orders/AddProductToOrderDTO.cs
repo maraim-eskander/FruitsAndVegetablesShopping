4550081ff346db15ff910fe026d1613a3ab14aad
Moaz715 <moazfathy592005@gmail.com>
2025-08-10 22:32:32 +0300

    Create AddProductToOrderDTO.cs

---
+++ FruitsAndVegetablesShopping.BLL/ModelVm/Orders/AddProductToOrderDTO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Group7_demo_BLL.ModelVM
+{
+    public class AddProductToOrderDTO
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
