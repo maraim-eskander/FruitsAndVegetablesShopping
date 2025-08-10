c33c8250cf66452ab79667204b13f2eeda5bfcc1
Moaz715 <moazfathy592005@gmail.com>
2025-08-10 22:29:26 +0300

    Update ProgramTest.cs

--- FruitsAndVegetablesShopping.DAL/Entities/TestIdentity/ProgramTest.cs
+++ FruitsAndVegetablesShopping.DAL/Entities/TestIdentity/ProgramTest.cs
@@ -1,9 +1,12 @@
-
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MVC_Group7_demo_BLL.Services.Abstraction;
+using MVC_Group7_demo_BLL.Services.Implementation;
 using MVC_Group7_demo_DAL.DataBase;
 using MVC_Group7_demo_DAL.Entities;
+using MVC_Group7_demo_DAL.Repository.Abstraction;
+using MVC_Group7_demo_DAL.Repository.Implementation;
 using System.Threading.Tasks;
 
 namespace MVC_Group7_demo_PLL
@@ -21,18 +24,38 @@ namespace MVC_Group7_demo_PLL
 
             builder.Services.AddDbContext<Context>(options => options.UseSqlServer(connectionString));
 
-            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
-                options =>
-                    {
-                        options.LoginPath = new PathString("/Account/Login");
-                        options.AccessDeniedPath = new PathString("/Account/Login");
-                    });
+            builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
+            .AddCookie(IdentityConstants.ApplicationScheme, options =>
+            {
+                options.LoginPath = "/Account/Login";
+                options.AccessDeniedPath = "/Account/AccessDenied";
+            });
 
             builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<Context>()
+                .AddSignInManager<SignInManager<ApplicationUser>>()
                 .AddTokenProvider<DataProtectorTokenProvider<ApplicationUser>>(TokenOptions.DefaultProvider);
+
+            builder.Services.AddScoped<IAdminRepo, AdminRepo>();
+            builder.Services.AddScoped<ICartRepo, CartRepo>();
+            builder.Services.AddScoped<ICategoryRepo, CategoryRepo>();
+            builder.Services.AddScoped<ICustomerRepo, CustomerRepo>();
+            builder.Services.AddScoped<IDeliveryRepo, DeliveryRepo>();
+            builder.Services.AddScoped<IOrdersRepo, OrdersRepo>();
+            builder.Services.AddScoped<IProductCartRepo, ProductCartRepo>();
+            builder.Services.AddScoped<IProductOrderRepo, ProductOrderRepo>();
+            builder.Services.AddScoped<IProductRepo, ProductRepo>();
+
+            //builder.Services.AddScoped<IAdminServices, AdminServices>();
+            builder.Services.AddScoped<ICartServices, CartServices>();
+            builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddScoped<ICustomerServices, CustomerServices>();
+            //builder.Services.AddScoped<IDeliveryServices, DeliveryServices>();
+            builder.Services.AddScoped<IOrdersServices, OrdersServices>();
+            //builder.Services.AddScoped<IProductCartServices, ProductCartServices>();
+            builder.Services.AddScoped<IProductOrderServices, ProductOrderServices>();
+            builder.Services.AddScoped<IProductService, ProductService>();
 
 
             var app = builder.Build();
