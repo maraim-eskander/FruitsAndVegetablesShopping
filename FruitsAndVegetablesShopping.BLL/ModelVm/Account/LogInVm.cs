8e74db388279aa2beda586d5e917df831877ddfa
Moaz715 <moazfathy592005@gmail.com>
2025-08-10 22:34:22 +0300

    Create LogInVm.cs

---
+++ FruitsAndVegetablesShopping.BLL/ModelVm/Account/LogInVm.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Group7_demo_BLL.ModelVM
+{
+    public class LogInVm
+    {
+        
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
